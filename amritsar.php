bf8214e04616b398085d4bc0493a8bdddc9b3314
Noman Sayyed <sayyednoman@gmail.com>
2016-11-09 13:27:29 +0530

    Add files via upload

---
+++ amritsar.php
@@ -0,0 +1,26 @@
+<?php include("emergency/pilot.php"); ?>
+ 
+	<div class="content">
+		<div class="row">
+			<div class="left-col">
+				<div class="doctors-profile clearfix">
+					<div class="doctor-profile-wrap">
+						<figure class="doctor-image monika-punj"></figure>
+						<div class="doctor-copy">
+							<h3>Dr. Monika Punj</h3>
+							<span class="doctor-desig">M.B.B.S, D.N.B. (Gynae)</span>
+							<span class="doctor-center">Manokamna IVF Center</span>
+							<p>Dr. Monica Punj is a core fertility specialist at Manokamna IVF Centre. She has done her MBBS from GMC Amritsar. She also did DNB Gynae from Sir Ganga Ram Hospital New Delhi. Dr. Punj have done senior Residency at Rajiv Gandhi cancer Institute Delhi &amp; ESI Hospital Basaidarapur. She has taken special training for fertility related endoscopic surgeries from SELSI. She did hands on training at embryology Academy for research &amp; training. Her Endeavour is to be involved in ongoing research programmes in the field of infertility &amp; achieve a level of excellence. Her main Motto is to know all the intricacies of infertility &amp; go into minor details of pit falls which make IVF fail. Our aim is to support infertile couple.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<?php include("emergency/right-col.php"); ?>			
+
+		</div>
+	</div>
+
+	<?php include("emergency/ebook.php"); ?>
+
+<?php include("emergency/footer-global.php"); ?>
---
+++ anand.php
@@ -0,0 +1,144 @@
+<?php include("emergency/pilot.php"); ?>
+ 
+	<div class="content">
+		<div class="row">
+			<div class="left-col">
+				<div class="doctors-profile clearfix">
+					<div class="doctor-profile-wrap">
+						<figure class="doctor-image dipan-thakkar"></figure>
+						<div class="doctor-copy">
+							<h3>Dr. Dipan Thakkar, Anand</h3>
+							<span class="doctor-desig">M.D. (OBS &amp; GYNE)</span>
+							<span class="doctor-center">Morpheus Usha Fertility Center</span>
+							<p>Dr. Dipan Thakkar is a leading Obstetrician &amp; Gynecologist having an experience of over 20 years in the field of Infertility, laparoscopic & hysteroscopic surgery, Ultrasound and high risk Obstetrics. He has received training from India's premier institutes for laparoscopy and Obstetric &amp; Gynec Ultrasound. He has attended many workshops and also presented papers in various National and regional conferences.</p>
+							<p>Dr. Thakkar was also the President of Anand Obstetric and Gynecological Society (affiliated to FOGSI) He is the Core Fertility Specialist at Anand Fertility Center, Anand and has received further training for IVF, Advanced infertility and human reproductive Endocrinology at Bad M&uuml;nder, Hamburg, Germany.</p>
+						</div>
+					</div>
+				</div>
+
+				<aside class="group clearfix">
+					<h2>IVF Packages</h2>
+					<div class="table-responsive">
+						<table class="table">
+							<thead>
+								<tr>
+									<th>Package</th>
+									<th>PPC</th>
+									<th>DYC</th>
+									<th>DYC+</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td><strong>No. of Cycle</strong></td>
+									<td>1</td>
+									<td>2</td>
+									<td>2</td>
+								</tr>
+								<tr>
+									<td><strong>Drugs Cost</strong></td>
+									<td>Extra</td>
+									<td>Extra</td>
+									<td>Extra</td>
+								</tr>
+								<tr>
+									<td><strong>Cryo Service</strong></td>
+									<td>Extra</td>
+									<td>Extra</td>
+									<td>Inclusive</td>
+								</tr>
+								<tr>
+									<td><strong>Blastocyst</strong></td>
+									<td>Extra</td>
+									<td>Extra</td>
+									<td>Inclusive</td>
+								</tr>
+								<tr>
+									<td><strong>Price</strong></td>
+									<td>&#8377; 75,000/-</td>
+									<td>&#8377; 1,00,000/-</td>
+									<td>&#8377; 1,25,000/-</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</aside>
+
+				<aside class="group clearfix fertility-services">
+					<h2>Fertility Services</h2>
+					<div class="tick-points mini-tick">
+						<ul>
+							<li>Confidential Counseling</li>
+							<li>IUI (Intra Uterine Insemination)</li>
+							<li>IVF (In vitro Fertilization)</li>
+							<li>ICSI (Intra Cytoplasmic Sperm Injection)</li>
+							<li>Blastocyst culture</li>
+							<li>Cryopreservation</li>
+							<li>Surgical sperm retrieval</li>
+							<li>Donor programs (Egg / sperm / embryo &amp; Surrogacy)</li>
+						</ul>
+					</div>
+				</aside>
+
+				<aside class="group clearfix ">
+					<h2>Doctor's Experience</h2>
+					<div class="doctors-experience">
+					<div id="doctors-experience" class="carousel fade" data-ride="carousel">
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<div class="testi">
+									<p>The clinic is also equipped with result oriented treatment plans with high standard of ethics and transparency.</p>
+									<div class="profile">
+										<img src="<?=$siteurl?>images/male.jpg" class="avatar">
+										<div class="profile-info">
+											<span class="name">Dr. Rama Shrivastava</span>
+											<span class="degree">MD(Obs &amp; Gyn)</span>
+										</div>
+									</div>
+								</div>
+								<div class="testi">
+									<p>I am very much happy with the services given by Morpheus IVF and Dr. Dipen Sir. My patients have been very happy &amp; satisfied too.</p>
+									<div class="profile">
+										<img src="<?=$siteurl?>images/male.jpg" class="avatar">
+										<div class="profile-info">
+											<span class="name">Dr. Alpa Shah</span>
+											<span class="degree">MD.(Gynaec)</span>
+											<span class="other">Mahavir Marg, Anand.</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item">
+								<div class="testi">
+									<p>I appreciate the best ethical practices and good treatment to our valued patients.</p>
+									<div class="profile">
+										<img src="<?=$siteurl?>images/male.jpg" class="avatar">
+										<div class="profile-info">
+											<span class="name">Dr. C.D. Vaghela</span>
+											<span class="degree">MD (Gyn &amp; Obst)</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<a class="left carousel-control" href="#doctors-experience" role="button" data-slide="prev">
+						<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#doctors-experience" role="button" data-slide="next">
+						<span class="sr-only">Next</span>
+						</a>
+					</div>
+					</div>
+				</aside>
+
+			</div>
+			
+			<?php include("emergency/right-col.php"); ?>			
+
+		</div>
+	</div>
+
+	<?php include("emergency/ebook.php"); ?>
+
+<?php include("emergency/footer-global.php"); ?>
